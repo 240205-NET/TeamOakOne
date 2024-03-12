c289ddc9a61f8d5d74e7887afae14dd6d03e8f95
submergednabin <submergednabin@gmail.com>
2024-03-12 19:09:59 -0400

    adding key in gitignore

--- Toasted.Front/toasted-app/src/environments/environment.development.ts
+++ Toasted.Front/toasted-app/src/environments/environment.development.ts
@@ -1,3 +1,7 @@
 export const environment = {
-    
+    openWeatherKey:"959629c3080bddc2fe4095868c40e0fe",
+    countryCode:"US",
+    baseUrlApi: "https://api.openweathermap.org/",
+    units:"imperial", //imperial = Fahrenheit and metric for celcius
+    googleApi: 'AIzaSyA1SVwd2LBG8m1Y8OrEVVvJX7b996XyQyI'
 };
