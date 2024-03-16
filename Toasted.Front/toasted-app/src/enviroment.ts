62b1059064e7449a8e6e3a85e3a5d5e8354f9e56
submergednabin <submergednabin@gmail.com>
2024-03-16 00:36:21 -0400

    only environemt

--- Toasted.Front/toasted-app/src/enviroment.ts
+++ Toasted.Front/toasted-app/src/enviroment.ts
@@ -1,4 +1,8 @@
 export const environment = {
     production: false,
-    openWeatherKey: 'ee6705d32b2133792147c00b0fa88b82'
-    };
+    openWeatherKey:"959629c3080bddc2fe4095868c40e0fe",
+    countryCode:"US",
+    baseUrlApi: "https://api.openweathermap.org/",
+    units:"metric", //imperial = Fahrenheit and metric for celcius
+    googleApi: 'AIzaSyA1SVwd2LBG8m1Y8OrEVVvJX7b996XyQyI'
+};
