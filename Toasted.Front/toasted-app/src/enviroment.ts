8dedcfc3c93656e686c916d1927a7e5eee9fdb3a
submergednabin <submergednabin@gmail.com>
2024-03-19 11:10:31 -0400

    some minor css change on weather details components

--- Toasted.Front/toasted-app/src/enviroment.ts
+++ Toasted.Front/toasted-app/src/enviroment.ts
@@ -1,3 +1,11 @@
 export const environment = {
-    
+    production: false,
+    openWeatherKey:"",
+    countryCode:"US",
+    baseUrlApi: "https://api.openweathermap.org/",
+    units:"metric", //imperial = Fahrenheit and metric for celcius
+    googleApi: ''
 };
+
+
+    
