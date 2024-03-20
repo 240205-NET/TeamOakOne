0db733ada82da5e3cac48c8bb5272f58c1de37a7
submergednabin <submergednabin@gmail.com>
2024-03-20 13:40:53 -0400

    home page css added

--- Toasted.Front/toasted-app/src/enviroment.ts
+++ Toasted.Front/toasted-app/src/enviroment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-    openWeatherKey:"959629c3080bddc2fe4095868c40e0fe",
+    openWeatherKey:"",
     countryCode:"US",
     baseUrlApi: "https://api.openweathermap.org/",
     units:"metric", //imperial = Fahrenheit and metric for celcius
-    googleApi: 'AIzaSyA1SVwd2LBG8m1Y8OrEVVvJX7b996XyQyI'
+    googleApi: ''
 };
